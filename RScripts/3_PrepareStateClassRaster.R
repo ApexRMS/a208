2f15ba9d65cc1aacecd27b68ad052feed9182278
chloedebyser <chloe.debyser@mail.mcgill.ca>
2020-03-06 00:50:40 +0000

    Identified broad land cover and distinguised Pine/Other

--- RScripts/3_PrepareStateClassRaster.R
+++ RScripts/3_PrepareStateClassRaster.R
@@ -66,7 +66,7 @@ execGRASS("v.import", input=MPB_name, output='rawData_MPB', 'overwrite')
 execGRASS("v.import", input=CUTBLOCKS_name, output='rawData_CUTBLOCKS', 'overwrite')
 
       # From GRASS mapsets
-execGRASS('g.copy', raster=c('LC1990_agg_mask@PreparePrimaryStratum' ,'LC1990_agg_mask'))
+execGRASS('g.copy', raster=c('LC2010_agg_mask@PreparePrimaryStratum' ,'LC2010_agg_mask'))
 execGRASS('g.copy', vector=c('rawData_VRI@PreparePrimaryStratum' ,'rawData_VRI'))
 execGRASS('g.copy', raster=c('MASK@PreparePrimaryStratum' ,'MASK'))
 
@@ -92,10 +92,38 @@ res <- execGRASS('r.info', map='MASK', intern=T)[res_row] %>%
 rm(res_row, res_col)
 
       # Apply to region
+execGRASS('g.region', raster='MASK', res=as.character(res))
 execGRASS('g.region', zoom='MASK', res=as.character(res))
 
-#### Produce State Class raster ####
+#### Input 1 - Get broad Land Cover from AAFC 2010 LANDCOVER layer ####
+write.table(c('21=2', '25=2', '31=3', '41=4', '42=4', '45=4', '46=4', '51=5', '61=6', '61=6', '71=7', '73=7', '74=7', '91=9'), paste0(resultsDir, 'Tabular/Rules/StateClass_getBroadLandCover.txt'), sep="", col.names=FALSE, quote=FALSE, row.names=FALSE)
+execGRASS('r.reclass', input='LC2010_agg_mask', output='stateClass_broad_inter', rules=paste0(resultsDir, 'Tabular/Rules/StateClass_getBroadLandCover.txt'), flags=c('overwrite'))
+execGRASS('r.mapcalc', expression='stateClass_broad = stateClass_broad_inter', region='current', flags='overwrite')
+execGRASS('g.remove', type='raster', name='stateClass_broad_inter', flags='f')
 
+#### Input 2 - Distinguish between Pine and Other Forest using VRI ####
+# Add binary Pine/Other Forest column
+      # Add column
+execGRASS('v.db.addcolumn', map='rawData_VRI', layer='1', columns='Pine integer')
+
+      # Create sqlite statement
+rule <- paste('CASE WHEN (SPECIES_CD =', sQuote('PL'), 'OR SPECIES_CD =', sQuote('PLI'), ') AND (SPECIES_PC >= 25) THEN 1 ELSE 0 END')
+
+      # Populate column
+execGRASS('v.db.update', map='rawData_VRI', column='Pine', query_column=rule)
+
+# Rasterize
+execGRASS('v.to.rast', input='rawData_VRI', output='Pine_Other', use='attr', attribute_column='Pine', 'overwrite')
+
+#### Input 3 - Identify areas that experienced MPB or clearcuts -- PICK UP HERE ####
+# Identify areas that experienced MPB
+
+
+# Identify areas that experienced clearcuts
+
+
+#### Produce State Class raster ####
+# Combine all 3 inputs to produce State Class raster
 
 # Export
 execGRASS('r.out.gdal', input='stateClass', output=paste0(resultsDir, 'Spatial/DataLayers/StateClass.tif'), 'overwrite')
